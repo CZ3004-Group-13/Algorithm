9d9123a6f2e44a22acead7799d3e9beee87f0db6
TanJunHong <b190013@e.ntu.edu.sg>
2021-09-18 11:31:52 +0800

    Remove previous implementation and filter output properly

--- src/main/java/Reader.java
+++ src/main/java/Reader.java
@@ -1,8 +1,9 @@
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Reader {
     private static final String COMMAND = "C:\\darknet\\darknet-master\\build\\darknet\\x64\\darknet.exe detector demo data/yolov4.data cfg/yolov4_custom_test.cfg backup/yolov4_custom_train_final.weights -c 1 -thresh 0.9 -ext_output";
@@ -11,79 +12,53 @@ public class Reader {
     private static final String FPS = "FPS";
     private static final String SPACE = " ";
     private static final String EMPTY_STRING = "";
+    private static final Logger LOGGER = Logger.getLogger(Reader.class.getName());
 
     public static void main(String[] args) {
-        // readTextFile();
         processOutputDirectly();
-
     }
 
     private static void processOutputDirectly() {
          new Thread(() -> {
             try {
+                long timeNow = System.currentTimeMillis();
                 ProcessBuilder builder = new ProcessBuilder(COMMAND.split(SPACE));
                 builder.directory(new File(DIRECTORY));
                 builder.redirectErrorStream(true);
-                final Process process = builder.start();
-                BufferedReader in = new BufferedReader(new InputStreamReader(process.getInputStream()));
+                BufferedReader in = new BufferedReader(new InputStreamReader(builder.start().getInputStream()));
                 String line;
                 boolean isNextLines = false;
+                boolean isThereItems = false;
+                boolean startMessages = true;
                 while ((line = in.readLine()) != null) {
                     if (line.startsWith(FPS)) {
+                        if (isThereItems) {
+                            System.out.println();
+                            isThereItems = false;
+                        }
                         isNextLines = false;
                     }
                     if (isNextLines && !line.equals(EMPTY_STRING)) {
+                        isThereItems = true;
                         String[] items = line.split("[:)]");
                         int left = Integer.parseInt(items[2].trim().split(SPACE, 2)[0]);
                         int top = Integer.parseInt(items[3].trim().split(SPACE, 2)[0]);
                         int width = Integer.parseInt(items[4].trim().split(SPACE, 2)[0]);
                         int height = Integer.parseInt(items[5].trim().split("\\)", 2)[0]);
-                        System.out.println(left + SPACE + top + SPACE + width + SPACE + height);
+                        System.out.println(items[0] + " Left: " + left + " Top: " + top + " Width: " + width + " Height: " + height);
                     }
-                    if (line.startsWith(OBJECTS)) {
+                    if (line.startsWith(OBJECTS) && System.currentTimeMillis() - timeNow > 1000) {
+                        startMessages = false;
+                        timeNow = System.currentTimeMillis();
                         isNextLines = true;
+                    }
+                    if (startMessages) {
+                        LOGGER.log(Level.INFO, line);
                     }
                 }
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }).start();
-    }
-
-    private static void readTextFile() throws IOException, InterruptedException {
-        String currString = "?";
-        StringBuilder temp = new StringBuilder();
-
-        StringBuilder bufferTime = new StringBuilder();
-
-        while (!currString.equals(temp.toString())) {
-            currString = temp.toString();
-            temp = new StringBuilder();
-            BufferedReader objReader = new BufferedReader(new FileReader(DIRECTORY + "\\detections.txt"));
-            String strCurrentLine;
-            while ((strCurrentLine = objReader.readLine()) != null) {
-                temp.append(strCurrentLine).append(System.lineSeparator());
-                if (strCurrentLine.startsWith(OBJECTS)) {
-                    bufferTime = new StringBuilder();
-                } else {
-                    if (!strCurrentLine.equals(EMPTY_STRING)) {
-                        bufferTime.append(strCurrentLine).append(System.lineSeparator());
-                    }
-                }
-            }
-            System.out.println(bufferTime);
-
-            String[] charactersString = bufferTime.toString().split(System.lineSeparator());
-
-            for (String string: charactersString) {
-                String[] items = string.split("[:)]");
-                int left = Integer.parseInt(items[2].trim().split(SPACE, 2)[0]);
-                int top = Integer.parseInt(items[3].trim().split(SPACE, 2)[0]);
-                int width = Integer.parseInt(items[4].trim().split(SPACE, 2)[0]);
-                int height = Integer.parseInt(items[5].trim().split("\\)", 2)[0]);
-                System.out.println(left + SPACE + top + SPACE + width + SPACE + height);
-            }
-            Thread.sleep(1000);
-        }
     }
 }
