4aa4bdc778094a0ab27707fadc93da9759c83762
TanJunHong <b190013@e.ntu.edu.sg>
2021-09-18 12:32:21 +0800

    Add initial calibration

--- src/main/java/ImageDetectionProcessor.java
+++ src/main/java/ImageDetectionProcessor.java
@@ -12,13 +12,13 @@ public class ImageDetectionProcessor {
     private static final String EMPTY_STRING = "";
 
     public static void main(String[] args) {
-        new ImageDetectionProcessor().processOutputDirectly();
+        new ImageDetectionProcessor().RunImageRecognition();
     }
 
     /**
-     * Directly processes input from command line.
+     * Runs image recognition and directly processes input from command line.
      */
-    private void processOutputDirectly() {
+    private void RunImageRecognition() {
          new Thread(() -> {
             try {
                 long timeNow = System.currentTimeMillis();
@@ -48,6 +48,7 @@ public class ImageDetectionProcessor {
                         int top = Integer.parseInt(items[3].trim().split(SPACE, 2)[0]);
                         int width = Integer.parseInt(items[4].trim().split(SPACE, 2)[0]);
                         int height = Integer.parseInt(items[5].trim().split("\\)", 2)[0]);
+                        processInput(left, width, height);
                         System.out.println(items[0] + " Left: " + left + " Top: " + top + " Width: " + width + " Height: " + height);
                     }
 
@@ -61,5 +62,45 @@ public class ImageDetectionProcessor {
                 e.printStackTrace();
             }
         }).start();
+    }
+
+    private void processInput(int left, int width, int height) {
+        if (left < 170) {
+            System.out.println("Image on the left!");
+        } else if (left < 346) {
+            System.out.println("Image on the centre!");
+        } else {
+            System.out.println("Image on the right!");
+        }
+
+        int area = width * height;
+
+        if (area > 100000) {
+            System.out.println("Image is about less than 15 cm away!");
+        } else if (area > 70000) {
+            System.out.println("Image is about 20 cm away!");
+        } else if (area > 45000) {
+            System.out.println("Image is about 25 cm away!");
+        } else if (area > 35000) {
+            System.out.println("Image is about 30 cm away!");
+        } else if (area > 28000) {
+            System.out.println("Image is about 35 cm away!");
+        } else if (area > 21000) {
+            System.out.println("Image is about 40 cm away!");
+        } else if (area > 17000) {
+            System.out.println("Image is about 45 cm away!");
+        } else {
+            System.out.println("Image is more than 50 cm away!");
+        }
+
+        double slantness = (double) height / width;
+
+        // System.out.println(slantness);
+
+        if (slantness < 1.25) {
+            System.out.println("Not slanted");
+        } else {
+            System.out.println("Very slanted");
+        }
     }
 }
